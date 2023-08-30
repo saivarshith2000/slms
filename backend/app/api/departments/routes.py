aa08e085f16f643562919bfada99142b99ee2b1f
Sai Varshith, Hariyala <hosvarshith@gmail.com>
2023-08-30 22:17:40 +0530

    added department routes and tests

--- backend/app/api/departments/routes.py
+++ backend/app/api/departments/routes.py
@@ -1,15 +1,18 @@
-
 from fastapi import APIRouter
 
 from app.db import DBSession
 
-from .schema import DepartmentSchema
-from .service import get_all_departments
+from .schema import DepartmentDetailSchema, DepartmentSchema
+from .service import get_all_departments, get_department_details
 
 router = APIRouter(prefix="/departments")
 
 
 @router.get("/all", response_model=list[DepartmentSchema])
 async def get_all_departments_route(session: DBSession):
-    depts = await get_all_departments(session)
-    return depts
+    return await get_all_departments(session)
+
+
+@router.get("/{code}", response_model=DepartmentDetailSchema)
+async def get_department_details_route(code: str, session: DBSession):
+    return await get_department_details(code, session)
--- backend/app/api/departments/service.py
+++ backend/app/api/departments/service.py
@@ -1,12 +1,15 @@
-from sqlalchemy import select
+from sqlalchemy import and_, func, select
 
 from app.db import DBSession
+from app.models.course import Course
 from app.models.department import Department
+from app.models.user import Role, User
 
 from .exceptions import department_not_found_exception
+from .schema import DepartmentDetailSchema, DepartmentSchema
 
 
-async def get_all_departments(session: DBSession) -> list[Department]:
+async def get_all_departments(session: DBSession) -> list[DepartmentSchema]:
     return await session.scalars(select(Department))
 
 
@@ -15,3 +18,18 @@ async def get_department_by_code(code: str, session: DBSession) -> Department:
     if not dept:
         raise department_not_found_exception(code)
     return dept
+
+
+async def get_department_details(code: str, session: DBSession) -> DepartmentDetailSchema:
+    dept = await get_department_by_code(code, session)
+    student_count = await session.scalar(
+        select(func.count()).select_from(User).where(and_(User.department_code == dept.code, User.role == Role.STUDENT))
+    )
+    course_count = await session.scalar(
+        select(func.count()).select_from(Course).where(Course.department_code == dept.code)
+    )
+    return DepartmentDetailSchema(
+        **dept.__dict__,
+        student_count=student_count,
+        course_count=course_count,
+    )
